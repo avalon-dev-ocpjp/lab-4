cbb899d15ac8e2600d1ba3e180e74c54c4d16616
Victor.Malmygin <Victor.Malmygin@192.168.11.172>
2019-03-01 15:28:49 +0300

    Realize main task

--- src/ru/avalon/java/dev/ocpjp/labs/Main.java
+++ src/ru/avalon/java/dev/ocpjp/labs/Main.java
@@ -4,6 +4,8 @@ import ru.avalon.java.dev.ocpjp.labs.models.Commodity;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -21,5 +23,54 @@ public class Main {
          * 7. Найти среднюю стоимость товаров
          * 8. Найти моду (медиану) стоимости товаров
          */
+        
+        //1
+        Commodity max = commodities.stream()
+                                    .max((c1, c2) -> Double.compare(c1.getPrice(), c2.getPrice()))
+                                    .get();
+        System.out.println("most expensive:\n" + max.getName() + " costs " + max.getPrice());
+        
+        //2
+        Commodity min = commodities.stream()
+                                    .min((c1, c2) -> Double.compare(c1.getPrice(), c2.getPrice()))
+                                    .get();
+        System.out.println("cheapest:\n" + min.getName() + " costs " + min.getPrice());
+        
+        //3
+        System.out.println("less then 5:");
+        Collection<Commodity> list = commodities.stream()
+                                                .filter((c) -> c.getResidue() < 5)
+                                                .collect(Collectors.toList());
+        list.forEach((c)->System.out.println(c.getName()));
+        
+        //4
+        System.out.println("total:");
+        Double sum = commodities.stream()
+                                .reduce(0.0,
+                                       (c1, c2) -> c1 + c2.getPrice()*c2.getResidue(),
+                                       (c1, c2) -> c1 + c2);
+        System.out.println(sum);
+        
+        //5
+        System.out.println("the longest name:");
+        commodities.stream()
+                    .reduce((c1, c2) -> (c1.getName().compareTo(c2.getName()) < 0 ? c1 : c2))
+                    .ifPresent((c) -> System.out.println(c.getName()));
+        
+        //6
+        commodities.stream()
+                   .sorted(Comparator.comparing(Commodity::getPrice))
+                   .collect(Collectors.toList());
+        
+        //7
+        System.out.println("average price:");
+        commodities.stream()
+                    .mapToDouble((c) -> c.getPrice())
+                    .average()
+                    .ifPresent(System.out::println);
+        
+        //8
+        System.out.println("median price:");
+        System.out.println((max.getPrice() - min.getPrice())/2);
     }
 }
